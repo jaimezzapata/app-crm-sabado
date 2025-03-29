99d0fa8ca043ab07831a3ca48417028df6bcc036
jaimezzapata <zapataval2304@gmail.com>
2025-03-29 16:09:31 -0500

    Se crea enrutador

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { enrutador } from './routes/enrutador'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import Home from './Home'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    <RouterProvider router={createBrowserRouter(enrutador)} />
   </StrictMode>,
 )
---
+++ src/routes/enrutador.jsx
@@ -0,0 +1,15 @@
+import Home from '../Home'
+import Login from '../pages/Login'
+
+export let enrutador = [
+    {
+        path: '/login',
+        element: <Login />
+    },
+    {
+        path: '/home',
+        element: <Home />
+    }
+]
+
+
